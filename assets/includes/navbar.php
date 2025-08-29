96f9399096ec81e2178203cb916cd20cdc5dc17e
Anurag <kkhanurag@gmail.com>
2025-08-29 16:46:21 +0000

    PHP In INDEX

--- assets/includes/navbar.php
+++ assets/includes/navbar.php
@@ -1,13 +1,15 @@
-<nav>
-  <div class="logo">
-    <a href="index.php"><img src="assets/images/logo.png" alt="Crimson Crown Logo"></a>
+<nav class="navbar">
+  <!-- Hamburger toggle -->
+  <div class="menu-toggle" id="menu-toggle">
+    ☰
   </div>
-  <ul>
+
+  <ul class="nav-links" id="nav-links">
     <li><a href="index.php">Home</a></li>
+    <li><a href="about.php">About</a></li>
     <li><a href="tournaments.php">Tournaments</a></li>
     <li><a href="leaderboard.php">Leaderboard</a></li>
     <li><a href="registration.php">Registration</a></li>
-    <li><a href="about.php">About</a></li>
     <li><a href="contact.php">Contact</a></li>
   </ul>
 </nav>
