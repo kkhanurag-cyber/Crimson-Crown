74fa088487a680ad932d629b630c3e157b376312
Anurag <kkhanurag@gmail.com>
2025-08-29 16:41:06 +0000

    PHP In INDEX

---
+++ assets/includes/footer.php
@@ -0,0 +1,4 @@
+<footer>
+  <p>© 2025 Crimson Crown. All rights reserved.</p>
+  <p>Follow us on <a href="https://discord.gg/yourinvite" target="_blank">Discord</a></p>
+</footer>
---
+++ assets/includes/navbar.php
@@ -0,0 +1,13 @@
+<nav>
+  <div class="logo">
+    <a href="index.php"><img src="assets/images/logo.png" alt="Crimson Crown Logo"></a>
+  </div>
+  <ul>
+    <li><a href="index.php">Home</a></li>
+    <li><a href="tournaments.php">Tournaments</a></li>
+    <li><a href="leaderboard.php">Leaderboard</a></li>
+    <li><a href="registration.php">Registration</a></li>
+    <li><a href="about.php">About</a></li>
+    <li><a href="contact.php">Contact</a></li>
+  </ul>
+</nav>
