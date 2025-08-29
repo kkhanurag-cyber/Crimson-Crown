227d14b0ea95a827bbf5ae0a4c63ad7d9e789a24
Anurag <kkhanurag@gmail.com>
2025-08-29 10:08:47 +0000

    updated Front-End Pages with common header/footer

--- assets/js/include.js
+++
@@ -1,9 +0,0 @@
-// Simple HTML include system for navbar and footer
-document.addEventListener("DOMContentLoaded", () => {
-  const includes = document.querySelectorAll('[data-include]');
-  includes.forEach(el => {
-    fetch(el.getAttribute('data-include'))
-      .then(resp => resp.text())
-      .then(data => el.innerHTML = data);
-  });
-});
