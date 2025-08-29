3639bc87248f1aafa7899a0c1656480aec4a25fb
Anurag <kkhanurag@gmail.com>
2025-08-29 17:06:40 +0000

    Added Nav CSS

--- assets/includes/footer.php
+++ assets/includes/footer.php
@@ -1,4 +1,2 @@
-<footer>
-  <p>© 2025 Crimson Crown. All rights reserved.</p>
-  <p>Follow us on <a href="https://discord.gg/yourinvite" target="_blank">Discord</a></p>
-</footer>
+footer>
+<p>© <?php echo date('Y'); ?> Crimson Crown. All rights
